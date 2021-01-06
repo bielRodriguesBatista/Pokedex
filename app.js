2a36fe28733c01146c2d1daa4242c21aa7bea455
bielRodriguesBatista <gabriel731rodrigues@gmail.com>
2021-01-06 15:57:30 -0300

    finishProject

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-const fethPokemon = () =>{
-    const getPokemonUrl = id =>  `https://pokeapi.co/api/v2/pokemon/${id}`
-    
-    //Array de promises
-    const pokemonPromises = []
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+//Array Constructor com 150 elementos preenchidos com undefined por causa do método fill
+// pois o constructor levanta 150 elementos porém eles não são visíveis
+
+//(underline) no método .map porque eu não vou usar o primeiro parâmetro
+const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
 
     //método fetch retorna uma promisses
-    for(let i = 1; i < 150; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-    }
-    Promise.all(pokemonPromises)
-    .then(pokemons => {
-        console.log(pokemons)
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-        // reduzir um array à algum outro tipo de dado
-        const liPokemons = pokemons.reduce((accumulator, pokemon) => {
-            accumulator += `
-            <li class="card">
-            <h2 class"card-title">${pokemon.id}. ${pokemon.name}</h2>
-            <p class"card-subtitle">${pokemon.types.map(typeinfo => typeinfo.type.name).join(' | ')}</p> 
-            </li>`
-        }, '')
-        // console.log(liPokemons)
-        // o parágrafo <p> retorna um array de string com os tipo de pokemon
-    })
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeinfo => typeinfo.type.name)
+
+    accumulator += `
+            <li class="card ${elementTypes[0]}">
+            <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
+            <h2 class"card-title">${id}. ${name}</h2>
+            <p class"card-subtitle">${elementTypes.join(' | ')}</p> 
+            </li>
+            `
+    return accumulator
+}, '')
+
+const insertePokemonsintoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
 }
-fethPokemon()
+
+//Array de promises
+const pokemonPromises = generatePokemonPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertePokemonsintoPage)
+
